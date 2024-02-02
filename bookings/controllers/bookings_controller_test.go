29d00b4c246ddaf92c4c644f400c5d3c7ba294af
luisalmas <la_luisalmas@hotmail.com>
2024-02-02 12:07:30 +0000

    Bookings controller tests (except delete)

--- bookings/controllers/bookings_controller_test.go
+++ bookings/controllers/bookings_controller_test.go
@@ -1,9 +1,19 @@
 package controllers
 
 import (
+	"encoding/json"
+	"errors"
+	"gym-management/bookings/models/dtos"
+	"gym-management/bookings/services"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
 type MockBookingsService struct {
@@ -15,7 +25,195 @@ func (mock *MockBookingsService) GetBookings() error {
 	return args.Error(0)
 }
 
-func TestGetBookings(t *testing.T) {
-	mockBookingsService := new(MockBookingsService)
-	mockBookingsService.On("GetBookings").Return(nil)
+var firstbooking = dtos.BookingCompleteDTO{
+	BookingId: 1,
+	Name: "Peter",
+	Date: time.Date(2024, time.January, 25,  0, 0, 0, 0, time.UTC),
+	ClassId: 1,
 }
+
+var secondbooking = dtos.BookingCompleteDTO{
+	BookingId: 2,
+	Name: "Samantha",
+	Date: time.Date(2024, time.January, 26,  0, 0, 0, 0, time.UTC),
+	ClassId: 1,
+}
+
+var bookingToInsert = &dtos.BookingDTO{
+	Name: "Dany",
+	Date: time.Date(2024, time.January, 27,  0, 0, 0, 0, time.UTC),
+	ClassId: 1,
+}
+
+var insertedbooking = &dtos.BookingCompleteDTO{
+	BookingId: 4,
+	Name: "Dany",
+	Date: time.Date(2024, time.January, 27,  0, 0, 0, 0, time.UTC),
+	ClassId: 1,
+}
+
+func setupController() (*BookingsControllerImpl, *gin.Engine) {
+	controller := NewBookingsController()
+	router := gin.Default()
+	prefix := router.Group("/api")
+	
+	controller.SetupRoutes(prefix)
+
+	return controller, router
+}
+
+func TestGetBookings(t *testing.T) {
+	controller, router := setupController()
+
+	bookingsMock := new(services.MockBookingsService)
+	bookingsMock.On("GetBookings").Return(&[]dtos.BookingCompleteDTO{
+		firstbooking,
+		secondbooking,
+	})
+	controller.BookingsService = bookingsMock
+
+	req, _ := http.NewRequest("GET", "/api/bookings", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusOK, resp.Code)
+	var response []dtos.BookingCompleteDTO
+	err := json.Unmarshal(resp.Body.Bytes(), &response)
+	require.NoError(t, err)
+	require.Equal(t, []dtos.BookingCompleteDTO{
+		firstbooking,
+		secondbooking,
+	}, response)
+}
+
+func TestGetBooking(t *testing.T) {
+	controller, router := setupController()
+
+	bookingsMock := new(services.MockBookingsService)
+	bookingsMock.On("GetBooking", mock.AnythingOfType("int")).Return(&firstbooking, nil)
+	controller.BookingsService = bookingsMock
+
+	req, _ := http.NewRequest("GET", "/api/bookings/1", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusOK, resp.Code)
+	var response dtos.BookingCompleteDTO
+	err := json.Unmarshal(resp.Body.Bytes(), &response)
+	require.Nil(t, err)
+	require.Equal(t, firstbooking, response)
+
+	bookingsMock = new(services.MockBookingsService)
+	bookingsMock.On("GetBooking", mock.AnythingOfType("int")).Return(nil, errors.New("error"))
+	controller.BookingsService = bookingsMock
+
+	req, _ = http.NewRequest("GET", "/api/bookings/99", nil)
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusNotFound, resp.Code)
+
+	req, _ = http.NewRequest("GET", "/api/bookings/asd", nil)
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusBadRequest, resp.Code)
+}
+
+func TestPostBookings(t *testing.T) {
+	controller, router := setupController()
+
+	req, _ := http.NewRequest("POST", "/api/bookings", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusBadRequest, resp.Code)
+
+	bookingsMock := new(services.MockBookingsService)
+	bookingsMock.On("InsertNewBooking", bookingToInsert).Return(insertedbooking, nil)
+	controller.BookingsService = bookingsMock
+
+	jsonData, errorJson := json.Marshal(bookingToInsert)
+	require.Nil(t, errorJson)
+	req, _ = http.NewRequest("POST", "/api/bookings", strings.NewReader(string(jsonData)))
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusCreated, resp.Code)
+	var response dtos.BookingCompleteDTO
+	err := json.Unmarshal(resp.Body.Bytes(), &response)
+	require.Nil(t, err)
+	require.Equal(t, *insertedbooking, response)
+
+	bookingsMock = new(services.MockBookingsService)
+	bookingsMock.On("InsertNewBooking", bookingToInsert).Return(nil, errors.New("error"))
+	controller.BookingsService = bookingsMock
+
+	jsonData, errorJson = json.Marshal(bookingToInsert)
+	require.Nil(t, errorJson)
+	req, _ = http.NewRequest("POST", "/api/bookings", strings.NewReader(string(jsonData)))
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusBadRequest, resp.Code)
+}
+
+func TestPutBookings(t *testing.T){
+	controller, router := setupController()
+
+	req, _ := http.NewRequest("PUT", "/api/bookings/1", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusBadRequest, resp.Code)
+
+	jsonData, errorJson := json.Marshal(bookingToInsert)
+	require.Nil(t, errorJson)
+	req, _ = http.NewRequest("PUT", "/api/bookings/sdf", strings.NewReader(string(jsonData)))
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusBadRequest, resp.Code)
+
+	bookingsMock := new(services.MockBookingsService)
+	bookingsMock.On("UpdateBooking", mock.AnythingOfType("int"), bookingToInsert).Return(insertedbooking, nil, nil)
+	controller.BookingsService = bookingsMock
+
+	jsonData, errorJson = json.Marshal(bookingToInsert)
+	require.Nil(t, errorJson)
+	req, _ = http.NewRequest("PUT", "/api/bookings/4", strings.NewReader(string(jsonData)))
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+	var response dtos.BookingCompleteDTO
+	err := json.Unmarshal(resp.Body.Bytes(), &response)
+
+	require.Nil(t, err)
+	require.Equal(t, http.StatusOK, resp.Code)
+	require.Equal(t, *insertedbooking, response)
+
+	bookingsMock = new(services.MockBookingsService)
+	bookingsMock.On("UpdateBooking", mock.AnythingOfType("int"), bookingToInsert).Return(nil, errors.New("error"), nil)
+	controller.BookingsService = bookingsMock
+
+	jsonData, errorJson = json.Marshal(bookingToInsert)
+	require.Nil(t, errorJson)
+	req, _ = http.NewRequest("PUT", "/api/bookings/4", strings.NewReader(string(jsonData)))
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusNotFound, resp.Code)
+
+	bookingsMock = new(services.MockBookingsService)
+	bookingsMock.On("UpdateBooking", mock.AnythingOfType("int"), bookingToInsert).Return(nil, nil, errors.New("error"))
+	controller.BookingsService = bookingsMock
+
+	jsonData, errorJson = json.Marshal(bookingToInsert)
+	require.Nil(t, errorJson)
+	req, _ = http.NewRequest("PUT", "/api/bookings/4", strings.NewReader(string(jsonData)))
+	resp = httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	require.Equal(t, http.StatusBadRequest, resp.Code)
+}
+
+//TODO delete
---
+++ bookings/services/bookings_service_mock.go
@@ -0,0 +1,101 @@
+package services
+
+import (
+	"gym-management/bookings/models/dtos"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type MockBookingsService struct {
+	mock.Mock
+}
+
+func (mockBookingsService *MockBookingsService) GetBookings() *[]dtos.BookingCompleteDTO {
+	args := mockBookingsService.Called()
+	return args.Get(0).(*[]dtos.BookingCompleteDTO)
+}
+
+func (mockBookingsService *MockBookingsService) GetBooking(id int) (*dtos.BookingCompleteDTO, error) {
+	args := mockBookingsService.Called(id)
+
+	if args.Get(0) != nil && args.Get(1) != nil {
+		return args.Get(0).(*dtos.BookingCompleteDTO), args.Get(1).(error)
+	}
+
+	if args.Get(1) == nil{
+		return args.Get(0).(*dtos.BookingCompleteDTO), nil
+	}
+
+	if args.Get(0) == nil{
+		return nil, args.Get(1).(error)
+	}
+	
+	return nil, nil
+}
+
+func (mockBookingsService *MockBookingsService) InsertNewBooking(newBooking *dtos.BookingDTO) (*dtos.BookingCompleteDTO, error){
+	args := mockBookingsService.Called(newBooking)
+
+	if args.Get(0) != nil && args.Get(1) != nil {
+		return args.Get(0).(*dtos.BookingCompleteDTO), args.Get(1).(error)
+	}
+
+	if args.Get(1) == nil{
+		return args.Get(0).(*dtos.BookingCompleteDTO), nil
+	}
+
+	if args.Get(0) == nil{
+		return nil, args.Get(1).(error)
+	}
+	
+	return nil, nil
+}
+
+func (mockBookingsService *MockBookingsService) UpdateBooking(id int, updatedBooking *dtos.BookingDTO) (*dtos.BookingCompleteDTO, error, error){
+	args := mockBookingsService.Called(id ,updatedBooking)
+	
+	if args.Get(0) != nil && args.Get(1) != nil && args.Get(2) != nil{
+		return args.Get(0).(*dtos.BookingCompleteDTO), args.Get(1).(error), args.Get(2).(error)
+	}
+
+	if args.Get(0) != nil {
+		return args.Get(0).(*dtos.BookingCompleteDTO), nil, nil
+	}
+
+	if args.Get(1) != nil {
+		return nil, args.Get(1).(error), nil
+	}
+
+	if args.Get(2) != nil {
+		return nil, nil, args.Get(2).(error)
+	}
+	
+	return nil, nil, nil
+}
+
+func (mockBookingsService *MockBookingsService) DeleteBooking(id int) (*dtos.BookingCompleteDTO, error, error){
+	args := mockBookingsService.Called(id)
+	
+	if args.Get(0) != nil && args.Get(1) != nil && args.Get(2) != nil{
+		return args.Get(0).(*dtos.BookingCompleteDTO), args.Get(1).(error), args.Get(2).(error)
+	}
+
+	if args.Get(0) != nil {
+		return args.Get(0).(*dtos.BookingCompleteDTO), nil, nil
+	}
+
+	if args.Get(1) != nil {
+		return nil, args.Get(1).(error), nil
+	}
+
+	if args.Get(2) != nil {
+		return nil, nil, args.Get(1).(error)
+	}
+	
+	return nil, nil, nil
+}
+
+func (mockBookingsService *MockBookingsService) validateBooking(newBooking *dtos.BookingDTO) error {
+	//Its not needed
+	return nil
+}
